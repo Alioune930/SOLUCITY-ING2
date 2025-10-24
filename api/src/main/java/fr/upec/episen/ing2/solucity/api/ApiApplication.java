c577ff2a029e7e4c89ebb430f892bd544e04fabd
GlabGlab <claraadamaly@gmail.com>
2025-10-24 17:50:45 +0200

    build a simple “Hello World” application using Spring Boot

--- api/src/main/java/fr/upec/episen/ing2/solucity/api/ApiApplication.java
+++ api/src/main/java/fr/upec/episen/ing2/solucity/api/ApiApplication.java
@@ -2,12 +2,22 @@ package fr.upec.episen.ing2.solucity.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+
 }
+
