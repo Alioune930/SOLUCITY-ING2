0672cbafb3810e2bb3639c0971988b64fd41d2ee
GlabGlab <claraadamaly@gmail.com>
2025-10-24 16:14:05 +0200

    add spring project

---
+++ api/src/test/java/fr/upec/episen/ing2/solucity/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.ing2.solucity.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
