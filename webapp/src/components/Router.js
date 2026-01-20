2674e31fb09efbb1e81850211c8d150fe096dae7
Alioune930 <tiendrebeogoali32@gmail.com>
2026-01-20 16:46:15 +0100

    feat: US1.1 - Ajout MapPage, App, Navbar, Router et données honfleur-roads

--- webapp/src/components/Router.js
+++ webapp/src/components/Router.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";       // App.js est dans components
 import Sample from "./Sample";
-import Navbar from "./Navbar";
+import MapPage from "./MapPage";
 import NotFound from "./NotFound";
+import Navbar from "./Navbar";
 
-export default function Router () {
-    return (
-        <BrowserRouter>
-            <div>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<App />}/>
-                    <Route path="/sample" element={<Sample />}/>
-                    <Route path="*" element={<NotFound />}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
-};
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/sample" element={<Sample />} />
+          <Route path="/MapPage" element={<MapPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
