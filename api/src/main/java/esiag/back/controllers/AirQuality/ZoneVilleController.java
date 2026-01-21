f1bb90f9d406ed2754a0bc9a5437e4d9be07441d
GlabGlab <claraadamaly@gmail.com>
2026-01-21 23:44:08 +0100

    Add ZONES API path to back.js

--- api/src/main/java/esiag/back/controllers/AirQuality/ZoneVilleController.java
+++ api/src/main/java/esiag/back/controllers/AirQuality/ZoneVilleController.java
@@ -1,16 +1,11 @@
 package esiag.back.controllers.AirQuality;
 
-import esiag.back.repositories.AirQuality.ZoneVilleRepository;
 import esiag.back.services.AirQuality.ZoneVilleService;
-import esiag.back.services.sample.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
