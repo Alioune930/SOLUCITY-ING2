1c310f127e3d044ec6f6de94f299bd8f6a609bf1
GlabGlab <claraadamaly@gmail.com>
2026-01-22 21:20:48 +0100

    Delete proto-back

--- webapp/src/constants/back.js
+++ webapp/src/constants/back.js
@@ -1,6 +1,3 @@
 
 export const LOCAL_HOST = '/api';
-export const LOCAL_HOST_SAMPLE = LOCAL_HOST + '/sample/';
-export const GET_SAMPLES = LOCAL_HOST_SAMPLE + 'all'
-export const UPDATE_SAMPLES = LOCAL_HOST_SAMPLE + 'update'
 export const GET_ZONES = LOCAL_HOST + '/zones/';
