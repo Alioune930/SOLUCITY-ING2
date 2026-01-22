8e320be3d3fef685f07a32f146cb56bc37db6385
chadamaly <claraadamaly@gmail.com>
2026-01-22 01:57:05 +0100

    Class Entity

---
+++ api/src/main/java/esiag/back/models/AirQuality/CapteurPollution.java
@@ -0,0 +1,34 @@
+package esiag.back.models.AirQuality;
+
+import lombok.Data;
+import org.locationtech.jts.geom.Point;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "capteur_pollution")
+@Data
+public class CapteurPollution {
+
+    @Id
+    @Column(name = "id_capteur", length = 20)
+    private String idCapteurPollution;
+
+    @Column(nullable = false)
+    private String profil;
+
+    @Column(nullable = false)
+    private String ville;
+
+    @Column(nullable = false)
+    private Float latitude;
+
+    @Column(nullable = false)
+    private Float longitude;
+
+    @Column(columnDefinition = "GEOMETRY(POINT, 4326)")
+    private Point geom;
+}
