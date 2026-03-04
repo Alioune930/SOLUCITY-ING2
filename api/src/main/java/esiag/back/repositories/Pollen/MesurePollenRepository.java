871c3dbfa4db75c3f0848430c27fb00c20422770
Alioune930 <tiendrebeogoali32@gmail.com>
2026-03-04 17:44:26 +0100

    Modification de MesurePollenRepository pour recuperer les données par capteur

--- api/src/main/java/esiag/back/repositories/Pollen/MesurePollenRepository.java
+++ api/src/main/java/esiag/back/repositories/Pollen/MesurePollenRepository.java
@@ -9,9 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MesurePollenRepository extends JpaRepository<MesurePollen, Long> {
-    @Query(value =
-        "SELECT mp.id_capteur AS idCapteur, mp.type_pollen AS typePollen, mp.indice_floraison AS indiceFloraison, mp.saison AS saison, mp.timestamp AS timestamp FROM mesure_pollen mp " +
-        "JOIN zone_ville zv ON zv.id_capteur_pollen = mp.id_capteur " +
-        "WHERE zv.id = :zoneId", nativeQuery = true)
-    List<DonneePollenLight> findAllZoneMeasures(@Param("zoneId") Long zoneId);
+    @Query(value = "SELECT mp.id_capteur AS idCapteur, mp.type_pollen AS typePollen, mp.indice_floraison AS indiceFloraison, mp.saison AS saison, mp.timestamp AS timestamp FROM mesure_pollen mp JOIN zone_ville zv ON zv.id_capteur_pollen = mp.id_capteur " +
+                   "WHERE zv.id_capteur_pollen = :idCap", nativeQuery = true)
+    List<DonneePollenLight> findByCapteur(@Param("idCap") String idCapteur);
 }
