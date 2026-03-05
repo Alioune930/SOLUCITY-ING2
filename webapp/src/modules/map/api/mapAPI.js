40ff48f3b1844f9dbd32b517a7697cd079717c63
chadamaly <claraadamaly@gmail.com>
2026-03-05 19:18:43 +0100

    Add function to fetch pollen data from API

--- webapp/src/modules/map/api/mapAPI.js
+++ webapp/src/modules/map/api/mapAPI.js
@@ -1,7 +1,15 @@
-import {GET_ZONES} from "../../../constants/back";
+import {GET_DonneesPollen, GET_ZONES} from "../../../constants/back";
 
 export async function fetchZones() {
     const response = await fetch(GET_ZONES);
+    if (!response.ok) {
+        throw new Error('Erreur lors du chargement des zones');
+    }
+    return response.json();
+}
+
+export async function fetchPollen() {
+    const response = await fetch(GET_DonneesPollen);
     if (!response.ok) {
         throw new Error('Erreur lors du chargement des zones');
     }
