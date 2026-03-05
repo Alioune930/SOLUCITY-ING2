6e54c3eb1059045c06d2816c8608cd98ca75510b
Alioune930 <tiendrebeogoali32@gmail.com>
2026-03-05 05:19:47 +0100

    ajout de la requete native findLatestByCapteur dans MesurePollenRepository

--- api/src/main/java/esiag/back/repositories/Pollen/MesurePollenRepository.java
+++ api/src/main/java/esiag/back/repositories/Pollen/MesurePollenRepository.java
@@ -9,7 +9,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MesurePollenRepository extends JpaRepository<MesurePollen, Long> {
-    @Query(value = "SELECT mp.id_capteur AS idCapteur, mp.type_pollen AS typePollen, mp.indice_floraison AS indiceFloraison, mp.saison AS saison, mp.timestamp AS timestamp FROM mesure_pollen mp JOIN zone_ville zv ON zv.id_capteur_pollen = mp.id_capteur " +
-                   "WHERE zv.id_capteur_pollen = :idCap", nativeQuery = true)
-    List<DonneePollenLight> findByCapteur(@Param("idCap") String idCapteur);
+    @Query(value = "SELECT mp.id_capteur AS idCapteur, mp.type_pollen AS typePollen, mp.indice_floraison AS indiceFloraison, mp.saison AS saison, mp.timestamp AS timestamp FROM mesure_pollen mp " +
+                   "WHERE mp.id_capteur = :idCap " +
+                   "ORDER BY mp.timestamp DESC LIMIT 1", nativeQuery = true)
+    DonneePollenLight findLatestByCapteur(@Param("idCap") String idCapteur);
 }
