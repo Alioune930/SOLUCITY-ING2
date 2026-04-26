38fd60097696a52747eaa0276f84146653b93149
Alioune930 <tiendrebeogoali32@gmail.com>
2026-04-26 23:54:55 +0200

    Ajout de la fonction fetchSensitiveZones pour envoyer l'url au backend avec le profil sensible

--- webapp/src/modules/map/api/mapAPI.js
+++ webapp/src/modules/map/api/mapAPI.js
@@ -1,9 +1,25 @@
-import {GET_DonneesPollen, GET_ZONES} from "../../../constants/back";
+import {GET_DonneesPollen, GET_ZONES, GET_SENSITIVE_ZONES} from "../../../constants/back";
 
 export async function fetchZones() {
     const response = await fetch(GET_ZONES);
     if (!response.ok) {
         throw new Error('Erreur lors du chargement des zones');
+    }
+    return response.json();
+}
+
+export async function fetchSensitiveZones(profile) {
+    const paramètres = new URLSearchParams({
+        asthme: profile.isAsthmatic.toString(), 
+        type: profile.allergyType,
+        polluLvl: profile.pollutionLevel || "", 
+        pollenLvl: profile.pollenLevel || ""
+    });
+    
+    const response = await fetch(`${GET_SENSITIVE_ZONES}?${paramètres.toString()}`);
+    
+    if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des zones sensibles");
     }
     return response.json();
 }
@@ -15,4 +31,3 @@ export async function fetchPollen() {
     }
     return response.json();
 }
-
