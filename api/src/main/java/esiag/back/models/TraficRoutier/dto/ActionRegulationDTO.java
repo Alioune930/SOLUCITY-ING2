bf0bf8601fcb600ff6bde20f58f16efc1144e725
dieng25 <moussa.dieng@etu.u-pec.fr>
2026-05-16 23:17:53 +0200

    ActionRegulation entity and dto

---
+++ api/src/main/java/esiag/back/models/TraficRoutier/dto/ActionRegulationDTO.java
@@ -0,0 +1,15 @@
+package esiag.back.models.TraficRoutier.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ActionRegulationDTO {
+
+    private Long id;
+    private String typeAction;
+    private String description;
+    private LocalDateTime dateAction;
+    private String statut;
+    private Long tronconId;
+}
