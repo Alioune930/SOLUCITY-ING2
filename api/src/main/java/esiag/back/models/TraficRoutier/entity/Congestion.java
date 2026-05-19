5da79f0590de4175725a265d841a30fd64f3ba65
dieng25 <moussa.dieng@etu.u-pec.fr>
2026-05-19 16:39:48 +0200

    update entity Congestion

--- api/src/main/java/esiag/back/models/TraficRoutier/entity/Congestion.java
+++ api/src/main/java/esiag/back/models/TraficRoutier/entity/Congestion.java
@@ -1,5 +1,31 @@
 package esiag.back.models.TraficRoutier.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "congestion")
+@Data
 public class Congestion {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private CongestionNiveau niveau;
+
+    private double vitesseMoyenne;
+
+    private int nombreVehicules;
+
+    private double tauxOccupation;
+
+    private LocalDateTime dateCalcul;
+
+    @ManyToOne
+    @JoinColumn(name = "troncon_id")
+    private Troncon troncon;
 }
