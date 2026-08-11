cc95bca1cbff9e81c92170e30fc8b6b72410047c
dieng25 <moussa.dieng@etu.u-pec.fr>
2026-08-11 04:03:09 +0200

    Update TronconService.java

--- api/src/main/java/esiag/back/services/TraficRoutier/TronconService.java
+++ api/src/main/java/esiag/back/services/TraficRoutier/TronconService.java
@@ -3,6 +3,9 @@ package esiag.back.services.TraficRoutier;
 import esiag.back.models.TraficRoutier.entity.Troncon;
 import esiag.back.repositories.TraficRoutier.TronconRepository;
 import org.springframework.stereotype.Service;
+import esiag.back.models.TraficRoutier.dto.CoordonneeDTO;
+import esiag.back.models.TraficRoutier.dto.TronconCarteDTO;
+import esiag.back.models.TraficRoutier.entity.Coordonnee;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,5 +33,35 @@ public class TronconService {
 
     public void delete(Long id) {
         tronconRepository.deleteById(id);
+    }
+
+    public List<TronconCarteDTO> getTronconsPourCarte() {
+
+        List<Troncon> troncons = tronconRepository.findAll();
+
+        return troncons.stream().map(troncon -> {
+
+            TronconCarteDTO dto = new TronconCarteDTO();
+
+            dto.setId(troncon.getId());
+            // dto.setNom(troncon.getNom());
+            dto.setLongueur(troncon.getLongueur());
+
+            List<CoordonneeDTO> coordonneesDTO = troncon.getCoordonnees().stream().map(coordonnee -> {
+
+                CoordonneeDTO coordDTO = new CoordonneeDTO();
+
+                coordDTO.setLatitude(coordonnee.getLatitude());
+                coordDTO.setLongitude(coordonnee.getLongitude());
+
+                return coordDTO;
+
+            }).toList();
+
+            dto.setCoordonnees(coordonneesDTO);
+
+            return dto;
+
+        }).toList();
     }
 }
